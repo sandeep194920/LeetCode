f60a80b82428cda79c3f3cfe443702c8d17c3f5b
sandeep194920 <sandeepamaranath@gmail.com>
2024-02-04 15:07:05 -0500

    Linkedlist common operations

---
+++ LinkedList Common Operations/README.md
@@ -0,0 +1,18 @@
+Linked List is best understood if you first implement these methods on your own before proceeding to other problems. If
+you need help in understanding the concepts, then go to Udemy and login with sandeepamaranath@gmail.com to access Scott
+Barett course
+
+### Implement these in Python
+
+- Create a **Singly Linked List** (and Node) Constructor
+- Perform these operations
+    - Print
+    - Push (To end of list)
+    - Pop (From end of list)
+    - Unshift (Add to the beginning)
+    - Shift (Remove from the beginning)
+    - Get (Get from anywhere in the list)
+    - Set (Update the value in the list)
+    - Insert (Add to anywhere in the list)
+    - Remove (Delete from anywhere in the list)
+    - Reverse (Reverse the list)
---
+++ LinkedList Common Operations/linked_list.py
@@ -0,0 +1,152 @@
+# When we need to create a new node, we call this
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+        self.tail = self.head
+        self.length = 1
+
+    def print(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return self
+
+    def pop(self):
+        if not self.head:
+            return None
+
+        # this code is written below in else loop (edge case)
+        # if self.length == 1:
+        #     self.head = None
+        #     self.tail = None
+        #     return temp
+
+        else:
+            temp = self.head
+            prev = self.head
+            while temp.next:
+                prev = temp
+                temp = temp.next
+            self.tail = prev
+            self.tail.next = None
+            self.length -= 1
+            # edge case if we have one single node
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+
+        return temp.value
+
+    def unshift(self, value):
+        # inserting a new node at the beginning
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return self
+
+    def shift(self):
+        # remove node at the start
+        if not self.head:
+            return None
+        node = self.head
+        self.head = self.head.next
+        node.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return node
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return -1
+
+        temp = self.head
+        i = 0
+        while i < index:
+            temp = temp.next
+            i += 1
+        return temp.value
+
+    def set(self, index, value):
+        if index < 0 or index >= self.length:
+            return -1
+
+        i = 0
+        temp = self.head
+        while i < index:
+            temp = temp.next
+            i += 1
+        temp.value = value
+        return self
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return -1
+
+        new_node = Node(value)
+
+        if index == 0:
+            return self.unshift(value)
+
+        # 10 20 -> 30 40
+        #     -> 60 ->
+        temp = self.head
+        i = 0
+        while i < index - 1:
+            temp = temp.next
+            i += 1
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return self
+
+    def remove(self, index):
+        if index == 0:
+            return self.shift()
+        elif index < 0 or index >= self.length:
+            return -1
+        else:
+            # 10 20 40 50
+            temp = self.head
+            prev = self.head
+            i = 0
+
+            while i < index:
+                prev = temp
+                temp = temp.next
+                i += 1
+            prev.next = temp.next
+            temp.next = None
+            self.length -= 1
+            return self
+
+
+my_linked_list = LinkedList(7)
+my_linked_list.push(10)
+my_linked_list.push(20)
+my_linked_list.push(50)
+my_linked_list.remove(40)
+
+my_linked_list.print()
